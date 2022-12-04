554fc440ac2752ffb96a7c6fe5439224722ed63e
Deepak <83416346+deepakb9966@users.noreply.github.com>
2022-12-04 11:29:18 +0000

    Pending changes exported from your codespace

--- server/openapi_server/run.py
+++ server/openapi_server/run.py
@@ -1,17 +1,17 @@
 import os
 import json
-import sys
+# import sys
 
 
 def run_command(self):
     cmd = os.system(
-        "py generate.py -i ../vitals/{type} -r ../vitals/{type}/{type}_rules.json -o ../vitals/{type}/{type}_mock.json".format(
+        "python3 generate.py -i ../vitals/{type} -r ../vitals/{type}/{type}_rules.json -o ../vitals/{type}/{type}_mock.json".format(
             type=self))
 
     if cmd:
         return "failed", 401
     else:
-        vitals_path=r"C:\Users\deepa\Documents\GitHub\mock_data_generate-uing-pythonfaker\vitals"
-        data = open(r"{path}\{type}\{type}_mock.json".format(path=vitals_path,type=self))
+        vitals_path=r"/workspaces/mock_data_generate-uing-pythonfaker/vitals"
+        data = open(r"{path}/{type}/{type}_mock.json".format(path=vitals_path,type=self))
         d = json.load(data)
         return d, 201
